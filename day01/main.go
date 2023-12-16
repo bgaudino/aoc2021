6fa3d50cd2762011fbb846d9504ef42a8e4ee9ec
bgaudino <bgaudino@gmail.com>
2023-12-15 21:08:57 -0600

    Day 1

---
+++ day01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/bgaudino/go-helpers"
+)
+
+func main() {
+	file, _ := os.Open("../data/day01.txt")
+	scanner := bufio.NewScanner(file)
+	var depth int
+	i := 0
+	increases := 0
+	windowA := []int{}
+	windowB := []int{}
+	windowIncreases := 0
+	for scanner.Scan() {
+		newDepth, _ := strconv.Atoi(scanner.Text())
+		if i > 0 && newDepth > depth {
+			increases++
+		}
+		windowA = append(windowA, newDepth)
+		if i > 0 {
+			windowB = append(windowB, depth)
+		}
+		if len(windowA) == 4 {
+			windowA = windowA[1:]
+		}
+		if len(windowB) == 4 {
+			windowB = windowB[1:]
+		}
+		if i > 2 && helpers.Sum(windowA...) > helpers.Sum(windowB...) {
+			windowIncreases++
+		}
+		depth = newDepth
+		i++
+	}
+	fmt.Printf("Part 1: %v\n", increases)
+	fmt.Printf("Part 2: %v\n", windowIncreases)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.19
+
+require (
+	github.com/bgaudino/go-helpers v0.0.0-20230702205349-42493b2d33cc // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+)
