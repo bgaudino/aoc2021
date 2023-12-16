5f376abe2238b1eeefe179674b4798417d66f0bb
bgaudino <bgaudino@gmail.com>
2023-12-15 22:25:43 -0600

    Day 3

---
+++ day03/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/bgaudino/go-helpers"
+)
+
+func mostAndLeastCommon(report []string, position int) (byte, byte) {
+	c := helpers.NewCounter([]byte{})
+	for _, number := range report {
+		c.Add(number[position])
+	}
+	sorted := c.MostCommon(-1)
+	most, least := sorted[0], sorted[len(sorted)-1]
+	if most.Count == least.Count {
+		return '1', '0'
+	}
+	return most.Element, least.Element
+}
+
+func main() {
+	file, _ := os.Open("../data/day03.txt")
+	scanner := bufio.NewScanner(file)
+	report := []string{}
+	for scanner.Scan() {
+		report = append(report, scanner.Text())
+	}
+
+	gammaRate := []byte{}
+	epsilonRate := []byte{}
+	l := len(report[0])
+	for i := 0; i < l; i++ {
+		most, least := mostAndLeastCommon(report, i)
+		gammaRate = append(gammaRate, most)
+		epsilonRate = append(epsilonRate, least)
+	}
+
+	i := 0
+	oxygenReport := report
+	for len(oxygenReport) > 1 {
+		j := i % l
+		most, _ := mostAndLeastCommon(oxygenReport, j)
+		oxygenReport = helpers.Filter(oxygenReport, func(n string) bool { return n[j] == most })
+		i++
+	}
+
+	i = 0
+	co2report := report
+	for len(co2report) > 1 {
+		j := i % l
+		_, least := mostAndLeastCommon(co2report, j)
+		co2report = helpers.Filter(co2report, func(n string) bool { return n[j] == least })
+		i++
+	}
+
+	gammaRateDecimal, _ := strconv.ParseInt(string(gammaRate), 2, 64)
+	epsilonRateDecimal, _ := strconv.ParseInt(string(epsilonRate), 2, 64)
+	oxygenGeneratorDecimal, _ := strconv.ParseInt(string(oxygenReport[0]), 2, 64)
+	co2scrubberDecimal, _ := strconv.ParseInt(string(co2report[0]), 2, 64)
+
+	fmt.Printf("Part 1: %v\n", gammaRateDecimal*epsilonRateDecimal)
+	fmt.Printf("Part 2: %v\n", oxygenGeneratorDecimal*co2scrubberDecimal)
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module main
 
 go 1.19
 
-require (
-	github.com/bgaudino/go-helpers v0.0.0-20230702205349-42493b2d33cc // indirect
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
-)
+require github.com/bgaudino/go-helpers v0.0.0-20231216034141-d2c0c372dd63
+
+require golang.org/x/exp v0.0.0-20231214170342-aacd6d4b4611 // indirect
