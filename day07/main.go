0af22b2aeddd9ea0c699e8d9ad1646b5e9f35b19
bgaudino <bgaudino@gmail.com>
2023-12-18 20:44:48 -0600

    Day 6

---
+++ day07/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/bgaudino/go-helpers"
+)
+
+func count(f helpers.Dict[int, int]) int {
+	return helpers.Sum(f.Values()...)
+}
+
+func main() {
+	file, _ := os.Open("../data/day06.txt")
+	scanner := bufio.NewScanner(file)
+	fish := make(helpers.Dict[int, int])
+	for scanner.Scan() {
+		text := scanner.Text()
+		for _, s := range strings.Split(text, ",") {
+			n, _ := strconv.Atoi(s)
+			fish[n]++
+		}
+	}
+	for i := 0; i < 256; i++ {
+		breeding := fish[0]
+		for j := 0; j < 8; j++ {
+			fish[j] = fish[j+1]
+		}
+		fish[6] += breeding
+		fish[8] = breeding
+		if i == 79 {
+			fmt.Printf("Part 1: %v\n", count(fish))
+		}
+	}
+	fmt.Printf("Part 2: %v\n", count(fish))
+}
